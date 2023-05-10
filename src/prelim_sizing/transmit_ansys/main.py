6e61257c9887ff9e2bd0eb646f55f61b1a697a30
msimoens <m.simoens@student.tudelft.nl>
2023-05-10 11:06:37 +0200

    Added calculation of laser flux

--- src/prelim_sizing/transmit_ansys/main.py
+++ src/prelim_sizing/transmit_ansys/main.py
@@ -1,3 +1,14 @@
 import transmit_methods as tm
+from src.plotting import plots as plt
+import numpy as np
 
+def main():
+    return None
+
+if __name__ = "__main__":
+
+
+    L = np.arange(10000,100000)
+    laser = tm.Laser(2.4e15, 0.25, 1, L)
+    plt.line_plot(L, laser.flux)
 
--- src/prelim_sizing/transmit_ansys/transmit_methods.py
+++ src/prelim_sizing/transmit_ansys/transmit_methods.py
@@ -1,28 +1,30 @@
 
 
 class Laser:
-    def __init__(self, dtr_eff, wavelength, av_mass):
+    def __init__(self, R_source, trans_eff, A_source, L):
         """
         :param dtr_eff: DC to RF efficiency [-]
         """
-        self.dtr_eff = dtr_eff
-        self.wavelength = wavelength
-        self.av_mass = av_mass
-        # self.
+        self.R_source = R_source
+        self.trans_eff = trans_eff
+        self.A_source = A_source
+        self.L = L
+        self.flux = self.calcflux()
         ...
 
     def __str__(self, ):
         return f"\nLaser:\n" \
                  f"------------------\n" \
-               f"    DC-to-RF efficiency: {self.dtr_eff} [-],\n" \
-               f"     Cell area: {self.cell_area} [m2],\n" \
-               f"  Average mass: {self.av_mass} [kg],\n" \
+               f"    Flux at receiver : {self.flux} [W/m2],\n" \
+
+               #f"    Cell area: {self.cell_area} [m2],\n" \
+               #f"  Average mass: {self.av_mass} [kg],\n" \
                # f" Power density: {self.power_dens} [W/m2],\n" \
                # f"Specific power: {self.spec_power} [W/kg]."
 
-    def calc_power_dens(self, ):
-
-        pass
+    def calcflux(self):
+        phi = self.R_source * self.A_source * self.trans_eff / self.L**2
+        return phi
 
 class Microwave:
     def __init__(self):

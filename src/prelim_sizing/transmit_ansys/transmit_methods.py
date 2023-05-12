d9deb55fa3a270bfe9990d58816606f618a99899
Mikhail Maximchuk <m.maximchuk@student.tudelft.nl>
2023-05-12 17:40:49 +0200

    Implemented Microwave class

--- src/prelim_sizing/transmit_ansys/transmit_methods.py
+++ src/prelim_sizing/transmit_ansys/transmit_methods.py
@@ -1,4 +1,24 @@
-from numpy import pi, sqrt, tan, exp
+import numpy as np
+
+LIGHT_SPEED = 3e8   # [m/s]
+
+
+def db2lin(db):
+    """
+    Args:
+        db: value in dB
+    Returns:
+        lin: value in linear
+    """
+    return 10 ** (db / 10)
+
+
+def calc_wavelength(freq):
+    """
+    Returns:
+        wavelength: wavelength of microwave [m]
+    """
+    return LIGHT_SPEED / freq
 
 
 class Laser:
@@ -21,7 +41,7 @@ class Laser:
         self.z = z
         # Calculated parameters
         self.calc_beam_div_angle()
-        self.flux_0 = self.P_0/(self.w0**2*pi)
+        self.flux_0 = self.P_0/(self.w0**2*np.pi)
         self.flux_z = self.calc_flux_dens_z()
 
     def __str__(self, ):
@@ -31,7 +51,7 @@ class Laser:
                f"    Beam quality factor M**2 : {self.m2} [-],\n" \
                f"    Distance to receiver : {self.z} [m],\n" \
                f"    Minimum beam width (close to transmitter) : {self.w0} [m],\n" \
-               f"    Minimum beam area (close to transmitter) : {self.w0**2*pi} [m^2],\n" \
+               f"    Minimum beam area (close to transmitter) : {self.w0**2*np.pi} [m^2],\n" \
                f"    Beam divergence angle (full) : {self.calc_beam_div_angle()} [rad],\n" \
                f"    Beam width at receiver: {self.calc_beam_width()} [m],\n" \
                f"    Beam area at receiver: {self.calc_area_z()} [m^2],\n" \
@@ -52,7 +72,7 @@ class Laser:
         """
         Returns: full divergence angle theta (NOT HALF)
         """
-        return 2 * self.m2 * self.wavelength / (pi * self.w0)
+        return 2 * self.m2 * self.wavelength / (np.pi * self.w0)
 
     def calc_beam_width(self):
         """
@@ -60,14 +80,14 @@ class Laser:
             w(z): width of beam at certain z [m]
         """
         # according to https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7885601:
-        return self.w0 * sqrt(1 + (self.wavelength * self.z / (pi * self.w0 ** 2)) ** 2)
+        return self.w0 * np.sqrt(1 + (self.wavelength * self.z / (np.pi * self.w0 ** 2)) ** 2)
 
     def calc_area_z(self):
         """
         Returns:
             A(z): beam area at certain z [m^2]
         """
-        return pi * self.calc_beam_width() ** 2
+        return np.pi * self.calc_beam_width() ** 2
 
     def calc_flux_dens_z(self):
         """
@@ -78,5 +98,63 @@ class Laser:
 
 
 class Microwave:
-    def __init__(self):
-        ...
+    def __init__(self, freq, eff_tx, eff_rx, db_gain_tx, db_gain_rx, spec_mass):
+        """
+        :param freq: frequency of microwave [Hz]
+        :param eff_tx: transmitter efficiency [-]
+        :param eff_rx: receiver efficiency [-]
+        :param db_gain_tx: transmitter gain [dB]
+        :param db_gain_rx: receiver gain [dB]
+        :param spec_mass: specific mass of transmitter [kg/W]
+        """
+        #  Frequency and wavelength
+        self.freq = freq
+        self.wavelength = calc_wavelength(freq)
+        #  Efficiencies
+        self.eff_tx = eff_tx
+        self.eff_rx = eff_rx
+        #  Gains
+        self.db_gain_tx = db_gain_tx
+        self.db_gain_rx = db_gain_rx
+        self.gain_tx = db2lin(db_gain_tx)
+        self.gain_rx = db2lin(db_gain_rx)
+        #  Specific mass
+        self.spec_mass = spec_mass
+
+    def __str__(self):
+        return f"\nMicrowave:\n" \
+               f"------------------\n" \
+               f"   Microwave frequency : {self.freq:.4e} [Hz],\n" \
+               f"  Microwave wavelength : {self.wavelength:.4e} [m],\n" \
+               f"Transmitter efficiency : {self.eff_tx} [-],\n" \
+               f"   Receiver efficiency : {self.eff_rx} [-],\n" \
+               f"      Transmitter gain : {self.db_gain_tx} [dB],\n" \
+               f"         Receiver gain : {self.db_gain_rx} [dB]."
+
+    def free_space_loss(self, distance):
+        """
+        Args:
+            distance: distance between transmitter and receiver [m]
+        Returns:
+            free space loss: [dB]
+        """
+        return (4 * np.pi * distance / self.wavelength) ** 2
+
+    def calc_power_tx(self, power_rx, distance):
+        """
+        Args:
+            power_rx: power at receiver [W]
+            distance: distance between transmitter and receiver [m]
+        Returns:
+            power_tx: power at transmitter [W]
+        """
+        return power_rx / (self.eff_rx * self.gain_rx * self.free_space_loss(distance) * self.gain_tx * self.eff_tx)
+
+    def calc_mass_tx(self, power_tx):
+        """
+        Args:
+            power_tx: power at transmitter [W]
+        Returns:
+            mass_tx: mass of transmitter [kg]
+        """
+        return power_tx * self.spec_mass

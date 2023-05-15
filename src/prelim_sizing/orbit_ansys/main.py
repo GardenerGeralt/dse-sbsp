8665bd062e77338b6298c7428a47739c526f07bc
daanschramade <daan.schramade@gmail.com>
2023-05-15 13:03:20 +0200

    Orbit calculations

--- src/prelim_sizing/orbit_ansys/main.py
+++ src/prelim_sizing/orbit_ansys/main.py
@@ -1,92 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 11 10:51:58 2023
-
-@author: daans
-"""
-from math import pi, sqrt, sin, cos, asin, tan, atan2
-import numpy as np
+import orbit_ansys as oa
 
 
-## Definitions ##
-# Functions
-def viewtime(e, theta_v):
-    E = 2 * atan2(sqrt((1 - e)) * tan(theta_v / 2), sqrt(1 + e))
-    if E < 0:
-        E0 = 2 * atan2(sqrt((1 - e)) * tan(pi / 2), sqrt(1 + e))
-        E = 2 * E0 + E
-    M = E - e * sin(E)
-    t = M / (2 * pi) * T
-    t_v = T - 2 * t
-    return t, t_v
+def main():
+    ...
 
 
-def viewaltitude(t, T):
-    t_array = np.arange(round(t), (round(T - t) + 100), 100)
-    h_array = []
-    for i in t_array:
-        print(i / T)
-        M = i / T * 2 * pi
-        E = 0
-        E_new = 0.1
-        counter = 0
-        while abs(E_new - E) > 0.001:
-            E = E_new
-            E_new = E - (E - e * sin(E) - M) / (1 - e * cos(E))
-            counter = counter + 1
-            if counter > 1000:
-                break
-        theta = 2 * atan2(tan(E_new / 2), sqrt((1 - e) / (1 + e)))
-        h_array.append(a * (1 - e ** 2) / (1 + e * np.cos(theta)) - R)
-    h_v = np.average(h_array)
-    return h_v
+if __name__ == "__main__":
+    FLLO = oa.OrbitFromPeri(110, 0.09547, 84.8, "Frozen low lunar orbit")
+    FELO = oa.OrbitFromPeri(750, 0.5, 48.46, "Frozen elliptical lunar orbit")
 
-
-def eclipsetime(a, e, R, T):
-    phi = asin(R / a)
-    t_e = phi / pi * T
-    return t_e
-
-
-# Moon constants
-R = 1737.4  # [km]
-mu = 4.90486959 * 10 ** 3  # [km^3/s^2]
-
-# Orbital parameters
-i = pi / 2  # [rad]
-e = 0.5  # [-]
-h_peri = 750  # [km]
-a = (R + h_peri) / (1 - e)  # [km]
-# h_apo = 750                                         #[km]
-# a = (R + h_apo)/(1 + e)                             #[km]
-
-# General calculations
-T = 2 * pi * sqrt(a ** 3 / mu)  # [s]
-h_peri = a * (1 - e) - R  # [km]
-h_apo = a * (1 + e) - R  # [km]
-l = a * (1 - e ** 2)
-b = sqrt(a * l)
-x = a * e - R
-y = b * sqrt(1 - (x / a) ** 2)
-theta_v = pi - atan2(y, R)
-
-# View time
-t, t_v = viewtime(e, theta_v)  # [s]
-
-# View altitude
-h_v = viewaltitude(t, T)
-
-# Eclipse time
-t_e = eclipsetime(a, e, R, T)  # [s]
-
-print("Eccentricity: " + str(e))
-print("Semi-major axis: " + str(round(a, 2)) + " km")
-print("Pericenter altitude: " + str(round(h_peri, 2)) + " km")
-print("Apocenter altitude: " + str(round(h_apo, 2)) + " km")
-print("Orbital period: " + str(round(T / 3600, 3)) + " hrs")
-print("Time in view: " + str(round(t_v / 3600, 3)) + " hrs")
-print("Percentage in view: " + str(round(t_v / T * 100, 2)) + " %")
-print("Average altitude while in view: " + str(round(h_v, 2)) + " km")
-print("Time in eclipse: " + str(round(t_e / 3600, 3)) + " hrs")
-print("Percentage in eclipse: " + str(round(t_e / T * 100, 2)) + " %")
-
+    print(FLLO)
+    print(FELO)
---
+++ src/prelim_sizing/orbit_ansys/orbit_ansys.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+MOON_RADIUS = 1737.4                            # [km]
+MOON_GRAV_PARAM = 4.90486959 * 10 ** 3          # [km^3/s^2]
+
+
+def sec2hrs(value):
+    return value / 3600
+
+
+def percentage(value1, value2):
+    return value1 / value2 * 100
+
+
+class Orbit:
+    def __init__(self, semi_maj_ax, eccentricity, inclination, name="Orbit"):
+        """
+        :param semi_maj_ax: orbit semi-major axis [km]
+        :param eccentricity: orbit eccentricity [-]
+        :param inclination: orbit inclination [deg]
+        :param name: name of the orbit
+        """
+        self.eccentricity = eccentricity
+        self.inclination = inclination
+        self.name = name
+
+        self.semi_maj_ax = semi_maj_ax
+        self.period = self.calc_period()
+        self.view_time,self.view_altitude = self.calc_view()
+        self.eclipse_time = self.calc_eclipse()
+
+    def calc_period(self):
+        return 2 * np.pi * np.sqrt(self.semi_maj_ax ** 3 / MOON_GRAV_PARAM)
+
+    def calc_altitude(self):
+        h_peri = self.semi_maj_ax * (1 - self.eccentricity) - MOON_RADIUS  # [km]
+        h_apo = self.semi_maj_ax * (1 + self.eccentricity) - MOON_RADIUS  # [km]
+        return h_peri, h_apo
+
+    def calc_view(self):
+        # View angle
+        l = self.semi_maj_ax * (1 - self.eccentricity ** 2)
+        b = np.sqrt(self.semi_maj_ax * l)
+        x = self.semi_maj_ax * self.eccentricity - MOON_RADIUS
+        y = b * np.sqrt(1 - (x / self.semi_maj_ax) ** 2)
+        theta_v =  np.pi - np.arctan2(y, MOON_RADIUS)
+
+        # View time
+        E = 2 * np.arctan2(np.sqrt((1 - self.eccentricity)) * np.tan(theta_v / 2), np.sqrt(1 + self.eccentricity))
+        if E < 0:
+            E0 = 2 * np.arctan2(np.sqrt((1 - self.eccentricity)) * np.tan(np.pi / 2), np.sqrt(1 + self.eccentricity))
+            E = 2 * E0 + E
+        M = E - self.eccentricity * np.sin(E)
+        t = M / (2 * np.pi) * self.period
+        view_time = self.period - 2 * t
+
+        # View altitude
+        resolution = 100
+        t_array = np.arange(round(t), (round(self.period - t) + resolution), resolution)
+        h_array = []
+        for i in t_array:
+            # print(i / T)
+            M = i / self.period * 2 * np.pi
+            E = 0
+            E_new = 0.1
+            counter = 0
+            while abs(E_new - E) > 0.001:
+                E = E_new
+                E_new = E - (E - self.eccentricity * np.sin(E) - M) / (1 - self.eccentricity * np.cos(E))
+                counter = counter + 1
+                if counter > 1000:
+                    break
+            theta = 2 * np.arctan2(np.tan(E_new / 2), np.sqrt((1 - self.eccentricity) / (1 + self.eccentricity)))
+            h_array.append(self.semi_maj_ax * (1 - self.eccentricity ** 2) / (1 + self.eccentricity * np.cos(theta)) - MOON_RADIUS)
+        view_altitude = np.average(h_array)
+        return view_time, view_altitude
+
+    def calc_eclipse(self):
+        phi = np.arcsin(MOON_RADIUS / self.semi_maj_ax)
+        return phi / np.pi * self.period
+
+
+class OrbitFromPeri(Orbit):
+    def __init__(self, pericenter, eccentricity, inclination, name="Orbit"):
+        self.pericenter = pericenter
+        self.eccentricity = eccentricity
+        self.semi_maj_ax, self.apocenter = self.calc_distance()
+
+        super().__init__(self.semi_maj_ax, eccentricity, inclination, name)
+
+    def __str__(self):
+        return f"\n{self.name}:\n" \
+               f"------------------\n" \
+               f"           Semi-major axis: {self.semi_maj_ax:.2f} [km],\n" \
+               f"           Eccentricity: {self.eccentricity} [-],\n" \
+               f"           Inclination: {self.inclination} [deg],\n" \
+               f"           Pericenter altitude: {self.pericenter:.2f} [km],\n" \
+               f"           Apocenter altitude: {self.apocenter:.2f} [km],\n" \
+               f"           Orbital period: {sec2hrs(self.period):.3f} [hrs],\n" \
+               f"           Time in view: {sec2hrs(self.view_time):.3f} [hrs],\n" \
+               f"           Percentage in view: {percentage(self.view_time,self.period):.2f} [%],\n" \
+               f"           Average altitude while in view: {self.view_altitude:.2f} [km].\n" \
+               f"           Time in view: {sec2hrs(self.eclipse_time):.3f} [hrs],\n" \
+               f"           Percentage in view: {percentage(self.eclipse_time, self.period):.2f} [%]."
+
+    def calc_distance(self):
+        semi_maj_ax = (MOON_RADIUS + self.pericenter) / (1 - self.eccentricity)
+        apocenter = semi_maj_ax * (1 + self.eccentricity) - MOON_RADIUS
+        return semi_maj_ax, apocenter
+
+
+class OrbitFromApo(Orbit):
+    def __init__(self, apocenter, eccentricity, inclination, name="Orbit"):
+        self.apocenter = apocenter
+        self.eccentricity = eccentricity
+        self.semi_maj_ax, self.pericenter = self.calc_distance()
+
+        super().__init__(self.semi_maj_ax, eccentricity, inclination, name)
+
+    def __str__(self):
+        return f"\n{self.name}:\n" \
+               f"------------------\n" \
+               f"           Semi-major axis: {self.semi_maj_ax:.2f} [km],\n" \
+               f"           Eccentricity: {self.eccentricity} [-],\n" \
+               f"           Inclination: {self.inclination} [deg],\n" \
+               f"           Pericenter altitude: {self.pericenter:.2f} [km],\n" \
+               f"           Apocenter altitude: {self.apocenter:.2f} [km],\n" \
+               f"           Orbital period: {sec2hrs(self.period):.3f} [hrs],\n" \
+               f"           Time in view: {sec2hrs(self.view_time):.3f} [hrs],\n" \
+               f"           Percentage in view: {percentage(self.view_time,self.period):.2f} [%],\n" \
+               f"           Average altitude while in view: {self.view_altitude:.2f} [km],\n" \
+               f"           Time in view: {sec2hrs(self.eclipse_time):.3f} [hrs],\n" \
+               f"           Percentage in view: {percentage(self.eclipse_time, self.period):.2f} [%]."
+
+    def calc_distance(self):
+        semi_maj_ax = (MOON_RADIUS + self.apocenter) / (1 + self.eccentricity)
+        pericenter = semi_maj_ax * (1 - self.eccentricity) - MOON_RADIUS
+        return semi_maj_ax, pericenter

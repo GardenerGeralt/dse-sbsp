d39ffa83569eb9917dd8169354ef5c75c5c5638f
daanschramade <daan.schramade@gmail.com>
2023-05-16 14:05:12 +0200

    Orbit calculations

--- src/prelim_sizing/orbit_ansys/main.py
+++ src/prelim_sizing/orbit_ansys/main.py
@@ -7,7 +7,7 @@ def main():
 
 if __name__ == "__main__":
     FLLO = oa.OrbitFromPeri(110000, 0.09547, 84.8, "Frozen low lunar orbit")
-    FELO = oa.OrbitFromPeri(750000, 0.5, 48.46, "Frozen elliptical lunar orbit")
+    FELO = oa.OrbitFromPeri(750000, 0.5, 47.87, "Frozen elliptical lunar orbit")
 
     print(FLLO)
     print(FELO)

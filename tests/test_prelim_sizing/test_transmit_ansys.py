94ab139a5ec2b4a30c53763001840d038463b9e2
jakub <jakubkorycki@protonmail.com>
2023-05-16 09:54:46 +0200

    wrote unit tests db2lin, calc_wavelength, calc_circ_area in transmit_ansys

--- tests/test_prelim_sizing/test_transmit_ansys.py
+++ tests/test_prelim_sizing/test_transmit_ansys.py
@@ -1,16 +1,21 @@
 import pytest
+from numpy import pi
 from src.prelim_sizing.transmit_ansys import transmit_methods as tm
 
 
 class TestGeneral:
     def test_db2lin(self):
-        ...
+        assert tm.db2lin(0) == 1
+        assert tm.db2lin(60) == 1e6
 
     def test_calc_wavelength(self):
-        ...
+        assert tm.calc_wavelength(1) == tm.LIGHT_SPEED
+        assert tm.calc_wavelength(3e9) == 0.1
+        assert tm.calc_wavelength(10e4) == 30000
 
     def test_circ_area(self):
-        ...
+        assert tm.calc_circ_area(0) == 0
+        assert tm.calc_circ_area(10) == pi * 5 ** 2
 
 
 class TestLaser:

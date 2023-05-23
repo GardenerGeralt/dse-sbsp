3682d74d96fbcf79b59e9548181bf543cf7b9efb
Alessandro Pulimeno <64032840+puliman@users.noreply.github.com>
2023-05-23 21:31:01 +0200

    Trying to fix

--- src/prelim_sizing/cost_ansys/production_cost.py
+++ src/prelim_sizing/cost_ansys/production_cost.py
@@ -8,15 +8,16 @@ class Production:
         self.mass = mass
         self.learning = 0.8
 
-    def t1cost(self, mass,n):
-        satmass = mass/n
+    def t1cost(self, mass, n):
+        satmass = mass / n
         # print(satmass)
-        t1cost = 283.5*satmass**0.716
+        t1cost = 283.5 * satmass ** 0.716
+        # t1cost = 90 * satmass
         # print(t1cost)
-        return t1cost*1000*1.27131
+        return t1cost * 1000 * 1.27131
 
     def getcost(self, mass, n):
-        cost = self.t1cost(mass, n)*n**(1+math.log(self.learning)/math.log(2))
+        cost = self.t1cost(mass, n) * n ** (1 + math.log(self.learning) / math.log(2))
         euros = cost
 
         return euros
--- tests/test_prelim_sizing/test_cost_pipeline.py
+++ tests/test_prelim_sizing/test_cost_pipeline.py
@@ -5,8 +5,9 @@ import src.prelim_sizing.complexity.field as fld
 
 class TestCostPipeline:
     costs = np.array([])
-    for i in range(1,5000):
-        pipeline = cstppl.CostPipeline(75400, i)
+    for i in range(1, 5000):
+        pipeline = cstppl.CostPipeline(63550, i)
         pipeline.populatecost()
         costs = np.append(costs, [pipeline.gettotalcost()])
     print(np.argmin(costs))
+    print('tot', costs[np.argmin(costs)]*1e-9)

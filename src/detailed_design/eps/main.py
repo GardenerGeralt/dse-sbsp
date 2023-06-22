9c7cf4e52c6a801ff1ea9130e171c09684826e1b
msimoens <m.simoens@student.tudelft.nl>
2023-06-22 23:20:03 +0200

    Updated

--- src/detailed_design/eps/main.py
+++ src/detailed_design/eps/main.py
@@ -6,7 +6,7 @@ import src.database.utils as dbu
 
 def sizing_EPS():
     """Sizing of the batteries"""
-    bus_power = pb(p_aocs_ecl=62+18.4+2, p_cdh_ecl=0, p_gnc_ecl=0, p_prop_ecl=0, p_ther_ecl=0, p_struct_ecl=0,
+    bus_power = pb(p_aocs_ecl=84.9, p_cdh_ecl=0, p_gnc_ecl=0, p_prop_ecl=0, p_ther_ecl=0, p_struct_ecl=0,
                    p_pow_ecl=0, p_aocs_peak= 650, p_cdh_peak= 0, p_gnc_peak= 0, p_prop_peak=0, p_ther_peak=0,
                    p_struct_peak=0, p_pow_peak=0).calc_eclipse_power_budget()
     # battery = bt(p_req=bus_power , t_eclipse= 0.9558, E_spec=392400, E_dens=4.428e+8,
@@ -26,7 +26,7 @@ def sizing_EPS():
 
     "Mass of the total EPS system"
     masses = pm(shunt_mass=2.4, cable_sp_mass=4.4, L_cables=20, batteries_mass=total_battery_mass, PMAD_mass=27,
-                dry_mass=669.31, harness_frac=0.03, n_conv=235, m_conv=0.215)
+                dry_mass=545.78, harness_frac=0.03, n_conv=160, m_conv=0.215)
     V_conv = masses.n_conv * 47.33e-6*1000
     total_EPS_mass = masses.calc_EPS_mass()
     harness_mass = masses.harness_mass_v2
